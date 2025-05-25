48d052a0e7306355bb1baae778220362db9e3be9
stephanofer <slowdown_4@hotmail.com>
2025-05-25 17:08:30 -0500

    Add new images and update styles for Navbar, Announce, and Hero sections
    - Added image1.webp, image2.webp, and image3.webp to assets.
    - Updated Announce component to change h1 text color to white.
    - Modified Navbar component to replace logo with text and adjusted styles.
    - Enhanced SectionContainer styles for better layout and responsiveness.
    - Updated index.astro to include Hero section with new content and buttons.
    - Created Hero section with a title, description, buttons, and image cards.
    - Changed global CSS foreground color to black and set link color to black.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sentry from "@sentry/astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sentry({
+      dsn: "https://6e7eeeca383931b0af6fde05095fbe28@o4508385501839360.ingest.us.sentry.io/4509364214431744",
+      sourceMapsUploadOptions: {
+        project: "ebanos-website",
+        authToken:
+          "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",
+      },
+      autoInstrumentation: {
+        requestHandler: true,
+      },
+    }),
+  ],
+});
