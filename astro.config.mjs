547b5dba189e8ca515c448e58276cd65d66141f8
stephanofer <slowdown_4@hotmail.com>
2025-09-08 22:31:34 -0500

    Add new SVG assets and update Hero section with new design elements
    - Added new SVG icons: heart, calendar, shield, and package.
    - Removed unused image3.webp and updated image imports in Hero.astro.
    - Enhanced Hero section with trust badge and experience points.
    - Updated styles for better layout and responsiveness.
    - Replaced Image components with img tags for specific images.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,18 @@
 import { defineConfig } from "astro/config";
 import sentry from "@sentry/astro";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-  integrations: [
-    sentry({
-      dsn: "https://6e7eeeca383931b0af6fde05095fbe28@o4508385501839360.ingest.us.sentry.io/4509364214431744",
-      sourceMapsUploadOptions: {
-        project: "ebanos-website",
-        authToken:
-          "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",
-      },
-      autoInstrumentation: {
-        requestHandler: true,
-      },
-    }),
-  ],
+  integrations: [sentry({
+    dsn: "https://6e7eeeca383931b0af6fde05095fbe28@o4508385501839360.ingest.us.sentry.io/4509364214431744",
+    sourceMapsUploadOptions: {
+      project: "ebanos-website",
+      authToken:
+        "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",
+    },
+    autoInstrumentation: {
+      requestHandler: true,
+    },
+  }), sitemap()],
 });
